595142128a6c2d84e115ad2205f413904eca6590
engboris <engboris@hotmail.fr>
2016-11-27 22:39:19 +0100

    Progression

--- protocol/protocol.c
+++ protocol/protocol.c
@@ -8,6 +8,8 @@ Format du protocole :
 - Le reste des donnees
   + Soit une taille de chaine suivie de la chaine concernee
   + Un entier (4 ou 8 caracteres)
+
+Le header correspond a la taille + le type
 */
 
 #include <stdio.h>
@@ -33,7 +35,7 @@ typedef enum message_type {
 			 FONCTIONS D'ENCODAGE
    --------------------------------------------- */
 
-const char* messagetype_to_string(message_type type) {
+const char* encodeType(message_type type) {
 	switch (type) {
 		case BCST_t: return "BCST"; break;
 		case PRVT_t: return "PRVT"; break;
@@ -65,10 +67,24 @@ char* encodeNumber(int n, int length) {
 	return buffer;
 }
 
-// 4 char : length <= 9999
-// 8 char : length <= 9999 9999
+int calculateMessageLength(int length_postheader) {
+	int length_header = 4+4;
+	int assumption = length_header+length_postheader;
+	if (assumption > 9999) {
+		if (assumption <= 99999999) {
+			return 8+4+length_postheader;
+		}
+		else {
+			perror("calculateMessageLength : length to large to encode");
+			exit(0);
+		}
+	}
+	return assumption;
+}
+
 protocol makeMessageHeader(int length, message_type type) {
 	int length_nbchar = 0;
+	char* numberEncoding;
 	if (length <= 9999)
 		length_nbchar = 4;
 	else if (length <= 99999999)
@@ -78,20 +94,38 @@ protocol makeMessageHeader(int length, message_type type) {
 		exit(0);
 	}
 	char* buffer = (char*)calloc(sizeof(char), length_nbchar+4);
-	if (length_nbchar == 4)
-		buffer = strcat(buffer, encodeNumber(length, 4));
-	else
-		buffer = strcat(buffer, encodeNumber(length, 8));
-	buffer = strcat(buffer, messagetype_to_string(type));
+	if (length_nbchar == 4) {
+		numberEncoding = encodeNumber(length, 4);
+		buffer = strcat(buffer, numberEncoding);
+	}
+	else {
+		numberEncoding = encodeNumber(length, 8);
+		buffer = strcat(buffer, numberEncoding);
+	}
+	buffer = strcat(buffer, encodeType(type));
+	free(numberEncoding);
+	return buffer;
+}
+
+// ------------------- Connexion -------------------
+protocol encodeConnexion(char* pseudo, char* tube) {
+	int size_pseudo = strlen(pseudo);
+	int size_tube = strlen(tube);
+	int length_postheader = size_pseudo+size_tube;
+	int total_length = calculateMessageLength(length_postheader);
+	char* buffer = (char*)calloc(sizeof(char), total_length);
+	char* header = makeMessageHeader(total_length, HELO_t);
+	buffer = strcat(buffer, header);
+	buffer = strcat(buffer, pseudo);
+	buffer = strcat(buffer, tube);
 	return buffer;
 }
 
 int main() {
-	int length = 15;
-	protocol header = makeMessageHeader(length, PRVT_t);
+	protocol connexion = encodeConnexion("engboris", "s1");
 	int i;
-	for (i = 0; i < length; i++) {
-		printf("[%c]", header[i]);
+	for (i = 0; i < calculateMessageLength(10); i++) {
+		printf("[%c]", connexion[i]);
 	}
 	return 0;
 }
